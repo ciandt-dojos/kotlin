9ff6255d0f6f1b31a7eed301994997c18f3e88b7
Jean Pimentel <jpimentel@ciandt.com>
2017-12-06 16:09:14 -0200

    Initial project structure

---
+++ project/test/com/ciandt/dojos/CalcTest.kt
@@ -0,0 +1,12 @@
+package com.ciandt.dojos
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CalcTest {
+
+    @Test
+    fun sum() {
+        assertEquals(3, Calc.sum(1, 2))
+    }
+}
